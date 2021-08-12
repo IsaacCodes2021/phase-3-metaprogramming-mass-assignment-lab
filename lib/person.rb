887e1b2cfe816330fd8b841d2a4890e0aa9808de
IsaacCodes2021 <84209008+IsaacCodes2021@users.noreply.github.com>
2021-08-12 15:08:33 -0700

    completed

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,39 @@
 class Person
-  # your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+  :weight, :handed, :complexion, :t_shirt_size,
+  :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(hsh)
+    @name = hsh[:name]
+    @birthday = hsh[:birthday]
+    @hair_color = hsh[:hair_color]
+    @eye_color = hsh[:hair_color]
+    @height = hsh[:height]
+    @weight = hsh[:weight]
+    @handed = hsh[:handed]
+    @complexion = hsh[:complexion]
+    @t_shirt_size = hsh[:t_shirt_size]
+    @wrist_size = hsh[:wrist_size]
+    @glove_size = hsh[:glove_size]
+    @pant_length = hsh[:pant_length]
+    @pant_width = hsh[:pant_width]
+  end
+
 end
+
+puts x = Person.new({
+  name: "Avi",
+  birthday: "01/29/1984",
+  hair_color: "brown",
+  eye_color: "brown",
+  height: "short",
+  weight: "good",
+  handed: "lefty",
+  complexion: "decent",
+  t_shirt_size: "medium",
+  wrist_size: "small",
+  glove_size: "normal",
+  pant_length: "32",
+  pant_width: "32"
+})
+puts x.name
